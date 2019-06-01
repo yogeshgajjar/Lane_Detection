a2ceaf0a8bb7959c978937d672586eb1fcb7f46b
Yogesh Gajjar <ygajjar@usc.edu>
2019-06-01 13:56:28 -0700

    Initial commit

---
+++ lane_detection.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import cv2
+import numpy as np
+
+
+def canny(image):
+
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+#cv2.cvtColor command changes color of the image. Takes 2 argument where one is the image which we want to change the color and another which changes from RGB2GRAY"""
+    blur = cv2.GaussianBlur(gray, (5,5), 0)
+#Using Gaussian Blur to reduce noise in our image"""
+    canny = cv2.Canny(blur, 50,150)
+#cv2.Canny(image, low_threshold, high_threshold) If the diference between low threshold and high threshold is high, less edges will be detected"""
+    return canny
+
+def display_lines(image, lines):
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+            x1,y1,x2,y2 = line.reshape(4)
+            cv2.line(line_image, (x1,y1), (x2,y2), (255,0,0), 10)
+    return line_image
+
+def region_of_interest(image):
+    height = image.shape[0]
+    # the command image.shape gives tuples(no of rows, no of column of a image)
+    polygons = np.array([[(200,height), (1100, height), (550, 250)]])
+    mask = np.zeros_like(image)
+    #makes matrix full of zeros with the same dimenstions as of image
+    cv2.fillPoly(mask, polygons, 255)
+    masked_image = cv2.bitwise_and(image, mask)
+    return masked_image
+
+
+
+image = cv2.imread('test_image.jpg')
+lane_image = np.copy(image)
+#THis is needed because any changes made in the arrays of image will lead to changes in the original image. So recommended to always have copy while working with image arrays"""
+canny = canny(lane_image)
+cropped_image = region_of_interest(canny)
+lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap = 5)
+line_image = display_lines(lane_image, lines)
+combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+cv2.imshow('result',combo_image)
+cv2.waitKey(0)
